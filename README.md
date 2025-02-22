df01e6dda5da28d9d1a13b6e4b311fe9503b889e
Paul Ferlitz <paul.ferlitz@hotmail.com>
2025-02-22 19:54:35 +0100

    updated example and README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# jNBT v0.2
+# jNBT v0.3
 <p align="center">
-  <img src="https://img.shields.io/badge/version-0.2-blue">
+  <img src="https://img.shields.io/badge/version-0.3-blue">
   <img src="https://img.shields.io/badge/minecraft-1.21.4 (Java)-green">
   <img src="https://img.shields.io/badge/java-21-red">
 </p>
@@ -11,9 +11,9 @@ This NBT library will in the near future be the base of my Anvil library (WIP) a
 
 ### Currently supports
 - Read operation
+- Adding, removing, finding and editing elements in the NBT file 
 - Java Minecraft NBT files (raw & gzip)
 ### WIP
-- Edit operations
 - Write operations
 - Java Minecraft NBT files (zlib DEFLATE)
 ### Future
@@ -31,7 +31,21 @@ This NBT library will in the near future be the base of my Anvil library (WIP) a
 NBTReader reader = new NBTReader(new File("./path/to/NBT/file.dat"));
 
 try {
-    System.out.println(reader.read());
+    // Fetch and print the whole NBT file
+    Collection_Tag root = reader.read();
+    System.out.println(root);
+
+    // Fetch and print the "Inventory" list
+    // Currently it doesn't matter if you use Tag or Tag<?>. The latter generates less warning though.
+    Tag<?> inventory = root.getTagByName("Inventory");
+    System.out.println(inventory);
+    
+    // Add a new item to the "Inventory" list
+    Tag_Compound diamondHoe = new Tag_Compound();
+    diamondHoe.addTag(new Tag_Int("count", 1));
+    diamondHoe.addTag(new Tag_Double("Slot", 1.0));
+    diamondHoe.addTag(new Tag_String("id", "minecraft:diamond_hoe"));
+    ((Collection_Tag) inventory).addTag(diamondHoe);
 } catch (IOException e) {
     throw new RuntimeException(e);
 }
--- src/main/java/me/paulferlitz/Example.java
+++ src/main/java/me/paulferlitz/Example.java
@@ -1,12 +1,11 @@
 package me.paulferlitz;
 
 import me.paulferlitz.IO.NBTReader;
-import me.paulferlitz.NBTTags.Tag;
-import me.paulferlitz.NBTTags.Tag_Compound;
-import me.paulferlitz.NBTTags.Tag_Short;
+import me.paulferlitz.NBTTags.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * Example class with an example on how the parser can be used.
@@ -24,8 +23,37 @@ public class Example
         NBTReader reader = new NBTReader(nbtFile);
         try
         {
-            Tag root = reader.read();
+            // Fetch and print the whole NBT file
+            Collection_Tag root = reader.read();
             System.out.println(root);
+
+            // Fetch and print the "Inventory" list
+            // Currently it doesn't matter if you use Tag or Tag<?>. The latter generates less warning though.
+            Tag<?> inventory = root.getTagByName("Inventory");
+            System.out.println(inventory);
+
+            // Add a new item to the "Inventory" list
+            Tag_Compound diamondHoe = new Tag_Compound();
+            diamondHoe.addTag(new Tag_Int("count", 1));
+            diamondHoe.addTag(new Tag_Double("Slot", 1.0));
+            diamondHoe.addTag(new Tag_String("id", "minecraft:diamond_hoe"));
+            ((Collection_Tag) inventory).addTag(diamondHoe);
+
+            // Remove an item from the "Inventory" list - accessing the inventory's data directly (getData array)
+            ((Collection_Tag) inventory).getData().remove(1);
+            // Remove an item from the "Inventory" list - accessing the inventory's data directly (iterator)
+            Iterator<Tag<?>> iterator = ((Collection_Tag) inventory).getData().iterator();
+            while (iterator.hasNext()) {
+                Tag<?> tag = iterator.next();
+                if (tag instanceof Tag_Compound invItem) {
+                    if (invItem.getTagByName("id").getData().equals("minecraft:diamond_hoe")) {
+                        iterator.remove();
+                        break;
+                    }
+                }
+            }
+            // Remove an element ("Inventory") from the root - using built in remover
+            root.removeTag(root.getTagByName("Inventory"));
         } catch (IOException e)
         {
             throw new RuntimeException(e);
